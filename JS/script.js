8068c61157e28e8885d72f17cfd02982b7b8e13a
Mateusz Bryczek <bryczek.mateusz02@gmail.com>
2022-11-11 18:46:49 +0100

    "remove tasks" added

--- JS/script.js
+++ JS/script.js
@@ -10,36 +10,58 @@
         },
     ];
 
+    const addNewTask = (newTaskContent) => {
+        tasks.push({
+            content: newTaskContent,
+        });
+        render();
+    };
+
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString +=`
-                <li ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+                <li 
+                    ${task.done ? "style=\"text-decoration: line-through\"" : ""}>
+                <button class="js-remove">usuń</button>
                     ${task.content}
                 </li>
                 `;
         }
         document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, taskIndex) => {
+            removeButton.addEventListener("click", () => {
+                removeTask(taskIndex);
+            });
+        });
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+        if (newTaskContent === "") {
+            return;
+        }
+        addNewTask(newTaskContent);
     };
 
     const init = () => {
         render();
 
-        const form=document.querySelector("js-form");
+        const form = document.querySelector(".js-form");
 
-        form.addEventListener("submit", (event) => {
-            event.preverntDefault();
-
-            const newTaskContent = document.querySelector("js-newTsks").ariaValueMax.trim();
-            if (newTaskContent === "") {
-                return;
-            }
-
-            tasks.push({
-                content: newTaskContent,
-            });
-        });
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
